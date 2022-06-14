6066a927cc986a22d7e50c1bd8152a590d1aa207
kadir <develioglu306@gmail.com>
2022-06-14 22:11:57 +0300

    Add .env

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const server = http.createServer(app);
-const port = 3213;
 app.use(
   cors({
     origin: 'https://aviator-game.vercel.app',
@@ -21,8 +20,8 @@ const io = new Server(server, {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-server.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+server.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 
   Socket({ io });
 });
