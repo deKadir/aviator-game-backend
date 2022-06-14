433e3efa1cf9e55e7f24b098962f56e291a2989e
kadir <develioglu306@gmail.com>
2022-06-14 21:58:45 +0300

    Fix origin bug

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const Socket = require('./services/Socket');
 const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 const server = http.createServer(app);
 const port = 8080 || 3213;
-const io = new Server(server, { cors: '*:*' });
+app.use(
+  cors({
+    origin: 'https://aviator-game.vercel.app/',
+  })
+);
+const io = new Server(server, { cors: 'https://aviator-game.vercel.app/' });
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
